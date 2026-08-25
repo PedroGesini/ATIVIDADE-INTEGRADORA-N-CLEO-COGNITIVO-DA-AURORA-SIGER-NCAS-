8c6f2ee800c9ae8e00fd9636ce56c053e052ae02
PedroGesini <ribeirogesinip@gmail.com>
2026-08-25 20:08:06 -0300

    monte estrutura projeto

---
+++ codigo_fonte.py
@@ -0,0 +1,71 @@
+# ====================================
+#      NCAS - AURORA SIGER
+# ====================================
+
+while True:
+
+    print(f"----- SEJA BEM-VINDO, {insertname} -----")
+
+    print("""
+        SISTEMA OPERACIONAL AURORA
+
+    1 - Cadastrar registro
+    2 - Consultar registros
+    3 - Consultar módulos
+    4 - Criar alerta
+    5 - Analisar alerta
+    6 - Consultar histórico
+    7 - Executar regra lógica
+    8 - Assistente inteligente
+    9 - Sair
+    """)
+
+    # PROGRAMA PRINCIPAL
+    try:
+        opcao = int(input("DIGITE A OPÇÃO DESEJADA: "))
+
+    except ValueError:
+        print("Entrada inválida! Por favor, digite um número.")
+        continue
+
+    match opcao:
+
+        case 1:
+            # Cadastrar registro
+            input("\nPressione ENTER para voltar ao menu...")
+
+        case 2:
+            # Consultar registros
+            input("\nPressione ENTER para voltar ao menu...")
+
+        case 3:
+            # Consultar módulos
+            input("\nPressione ENTER para voltar ao menu...")
+
+        case 4:
+            # Criar alerta
+            input("\nPressione ENTER para voltar ao menu...")
+
+        case 5:
+            # Analisar alerta
+            input("\nPressione ENTER para voltar ao menu...")
+
+        case 6:
+            # Consultar histórico
+            input("\nPressione ENTER para voltar ao menu...")
+
+        case 7:
+            # Executar regra lógica
+            input("\nPressione ENTER para voltar ao menu...")
+
+        case 8:
+            # Assistente inteligente
+            input("\nPressione ENTER para voltar ao menu...")
+
+        case 9:
+            print("Encerrando o Sistema Operacional Aurora. Até logo!")
+            break
+
+        case _:
+            print("Opção inválida.")
+            input("\nPressione ENTER para voltar ao menu...")
